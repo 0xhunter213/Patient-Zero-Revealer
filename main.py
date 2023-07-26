0aade8e96f0d1240bff1d95be2c8a1b30c0dce98
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-07-26 21:02:14 +0100

    Add WinRM connection event detecter

--- main.py
+++ main.py
@@ -29,8 +29,10 @@ def printing_event(event):
 
 def RDP_connections(user=None,ip_source=None,timestamp=None):
     '''
-        RDP connection for a specifc user parameter
+        Remote Desktop connections for a specifc user parameters
     '''
+    # RDP searching query
+
     search_query ={ 
     "bool":{
         "must":[
@@ -75,24 +77,53 @@ def RDP_connections(user=None,ip_source=None,timestamp=None):
         printing_event(event_4624_rdp)
         return event_4624_rdp
     else:
-        print(f"No RDP connections with this Parameters")
+        print(f"No RDP connections with this Parameters\n")
         return None
 
+def WinRM_connections(user=None,ip_source=None,timestamp=None):
+    '''
+        Windows Remote Management Connection depending to user or ip source of suspicious machine or with timestamp
+    '''
+    # winrm searching query 
+    search_query = {
+        "bool":[
+            {"must":{"event.code":"91"}} # victim machine receved winrm connection
+        ],
+        "filter":[
+            
+        ]
+    }
+    
+    if user != None:
+        # searching with user name (attacker)
+        search_query["bool"]["filter"].append({"term":{"winlog.event_data.user.name":user}})
+    
+    if ip_source != None:
+        # adding Ip address source of previous event
+        search_query["bool"]["filter"].append({"term":{"host.ip":ip_source}})
+    
+    if timestamp != None:
+        # searching with timestamp range
+        timeline = datetime.strptime(timestamp,"%Y-%m-%dT%H:%M:%S.%fZ") - timedelta(hours=24)
+        min_timestamp = timeline.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        search_query["bool"]["filter"].append({"range":{
+            "@timestamp":{
+                "lte":timestamp,
+                "gte":min_timestamp,
+            }
+        }})
+
+    event_winrm_rquest = event_searching(query=search_query)
+
+    if event_winrm_rquest != None:
+        printing_event(event_winrm_rquest)
+        return event_winrm_rquest
+    else:
+        print("No WinRM connections with this Paremeters\n")
+        return None
 
 print("WinRM Connections\n")
     # Looking for winrm connections
-search_query = {
-    "bool":{
-        "must":[
-            {"match":{"event.code":"91"}},
-            {"match":{"winlog.user.name":"user-pc0"}}
-        ]    
-    }
-}
-    
-event = event_searching(search_query)
-printing_event(event)
 
-RDP_connections(user="user-pc1")
 
 print("DONE ")
