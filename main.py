b36284315efdc81c6787ed3edb5f562fcf66e912
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-07-26 19:59:52 +0100

    Update: RDP connections Function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from elasticsearch import Elasticsearch
 from decouple import config
 from datetime import datetime, timedelta
+from elasticsearch_dsl import Search,Q
 # Import Deployment Secrets keys
  
 ELASTIC_PASSWORD = config("ELASTIC_PASSWORD")
 CLOUD_ID = config("CLOUD_ID")
 INDEX_PATTERN = 'winlogbeat-*'
-es = Elasticsearch(cloud_id=CLOUD_ID,basic_auth=("elastic",ELASTIC_PASSWORD))
+es = Elasticsearch(cloud_id=CLOUD_ID,http_auth=("elastic",ELASTIC_PASSWORD))
 
 def event_searching(query):
     r = es.search(index=INDEX_PATTERN,query=query)
@@ -17,82 +18,81 @@ def event_searching(query):
         return None
 
 
-"""
-******* RDP connection ********
-"""
+def printing_event(event):
+    try:
+        print(f'Timestamp        : {event["@timestamp"]}\n\
+                Username         : {event["user"]}\n\
+                Ip Address Host  : {event["host"]["ip"]}\n\
+                Ip Addres Source : {event["source"]}\n')
+    except:
+        print(event)
 
-# search query
-
-search_query ={ 
+def RDP_connections(user=None,ip_source=None,timestamp=None):
+    '''
+        RDP connection for a specifc user parameter
+    '''
+    search_query ={ 
     "bool":{
         "must":[
             {"match":{"event.code":"4624"}},
         ],
-        "filter":{
-             "term":{"winlog.event_data.TargetUserName":"user-pc1"} # attacker user name
-        },
+        "filter":[
+        ],
         "should":[
             {"match":{"winlog.event_data.LogonType":"10"}},
             {"match":{"winlog.event_data.LogonType":"7"}},
         ]
+        }
+    }
+
+    if user != None:
+        # searching with user name (attacker)
+        search_query["bool"]["filter"].append({"term":{"winlog.event_data.TargetUserName":user}})
+    
+    if ip_source != None:
+        # adding Ip address source of previous event
+        search_query["bool"]["filter"].append({"term":{"host.ip":ip_source}})
+    
+    if timestamp != None:
+        # searching with timestamp range
+        timeline = datetime.strptime(timestamp,"%Y-%m-%dT%H:%M:%S.%fZ") - timedelta(hours=24)
+        min_timestamp = timeline.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+        search_query["bool"]["filter"].append({"range":{
+            "@timestamp":{
+                "lte":timestamp,
+                "gte":min_timestamp,
+            }
+        }})
+
+    # searching results
+    # exisit item with event ID 4624 type 10 searched
+    # also we can use timestamp or machine `ip` `name` ...
+
+    event_4624_rdp = event_searching(query=search_query)
+
+
+    if event_4624_rdp != None: 
+        printing_event(event_4624_rdp)
+        return event_4624_rdp
+    else:
+        print(f"No RDP connections with this Parameters")
+        return None
+
+
+print("WinRM Connections\n")
+    # Looking for winrm connections
+search_query = {
+    "bool":{
+        "must":[
+            {"match":{"event.code":"91"}},
+            {"match":{"winlog.user.name":"user-pc0"}}
+        ]    
     }
 }
-# searching results
-# exisit item with event ID 4624 type 10 searched
-# also we can use timestamp or machine `ip` `name` ...
-
-event_4624_rdp = event_searching(query=search_query)
-
-if event_4624_rdp != None: 
-
-    print(f'last login RDP:\n\
-            Timestamp        : {event_4624_rdp["@timestamp"]}\n\
-            Username         : {event_4624_rdp["user"]}\n\
-            Ip Address Host  : {event_4624_rdp["host"]["ip"]}\n\
-            Ip Addres Source : {event_4624_rdp["source"]}\n')
-    event = event_4624_rdp
-    while event["host"]["ip"][1] != event["source"]["ip"]:
-        print(event["source"]["ip"])
-        # timestamp range of 24 hours 
-        timestamp = datetime.strptime(event["@timestamp"],"%Y-%m-%dT%H:%M:%S.%fZ") - timedelta(hours=24)
-        min_timestamp = timestamp.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
-    # find another logon rdp type 10 or 7 in order to follow traces of attacker
-        search_query = {
-                "bool":{
-                    "must":[
-                        {"match":{"event.code":"4624"}},        
-                    ],
-                    "filter":[
-                        {"term":{"host.ip":event["source"]["ip"]}},
-                        {"range":{
-                            "@timestamp":{
-                                "lte":event["@timestamp"],
-                                "gte":min_timestamp
-                            }
-                            
-                            }
-                        }
-                    ],
-                    "should":[
-                        {"match":{"winlog.event_data.LogonType":"10"}},
-                        {"match":{"winlog.event_data.LogonType":"7"}},
-                    ]
-                }
-            }
-        event_4624_rdp = event
-        event = event_searching(search_query)
-
-        if event == None or event["source"]["domain"] == "-":
-            break
-        
-
-    print(f'First Machine Infected :\n\
-            Timestamp        : {event["@timestamp"]}\n\
-            Username         : {event["user"]}\n\
-            Ip Address Host  : {event["host"]["ip"]}\n\
-            Ip Addres Source : {event["source"]}\n')
-        
-else:
-    print("\033[1;31;40m RDP logon for this User does not exist !")
-       
     
+event = event_searching(search_query)
+printing_event(event)
+
+RDP_connections(user="user-pc1")
+
+print("DONE ")
