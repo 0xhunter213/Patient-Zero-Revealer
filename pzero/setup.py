ed1b48693d8c2433223f5e4c1705672c7fae4ff9
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-08-14 00:14:02 +0100

    add install config

---
+++ pzero/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="pzero-revealer",
+    version="0.0.1",
+    description= "Detecting the First Infected Machine by Attackers Through Different Windows Connection Protocols Using Windows Event Logs",
+    author="0xhunter213",
+    author_email="m.seddaoui@esi-sba.dz",
+    packages=find_packages()
+
+)
