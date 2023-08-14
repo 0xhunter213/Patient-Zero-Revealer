0562946aaf82438efa55c4504f62a77015b8c56d
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-08-14 22:39:07 +0100

    Update index api

---
+++ backend/fetch_events.py
@@ -0,0 +1,66 @@
+from elasticsearch import Elasticsearch
+from Elk import event_searching
+from decouple import config
+import asyncio
+import re
+CLOUD_ID = config("CLOUD_ID")
+PASSWORD = config("ELASTIC_PASSWORD")
+
+def data_format(event):
+    assert event
+    data_notes = {
+        "id":event["user"]["domain"],
+        "label":event["winlog"]["computer_name"],
+        "title":event["host"]["hostname"],
+        "color": "blue",
+        "shape": "image",
+        "image":"https://cdn.icon-icons.com/icons2/595/PNG/512/Computer_icon-icons.com_55509.png",
+        "size": 40,
+        "ip":event["source"]["ip"],
+        "build":event["host"]["os"]["build"],
+        "os":event["host"]["os"]["name"],
+        "domain":event["source"]["domain"]
+    }
+
+    return data_notes
+
+async def retrieve_netwrok_tpoplogy(es):
+    """
+        fetching machines exist and different connections on the network from elastic
+    """
+    ip_addresses=[]
+    data = {"nodes":[],"egdges":[]}
+    search_query = {
+            "bool":{
+                "must":[{"match":{"event.code":"3"}}]
+            }
+    }
+    
+    events = event_searching(es,query=search_query,all=True)
+    
+    for evt in events:
+        ip_addresses = [item["ip"] for item in data["nodes"]]
+        if evt["source"]["ip"] not in ip_addresses and not re.search(r":",evt["source"]["ip"]):
+            data["nodes"].append(data_format(evt))
+    for evt in events:
+        if evt["source"]["ip"] in ip_addresses:
+            rst_ips = ip_addresses.copy()
+            rst_ips.pop(ip_addresses.index(evt["source"]["ip"]))
+            for ip_dest in rst_ips:
+                if ip_dest in evt["related"]["ip"] or ip_dest == evt["destination"]["ip"]:
+                    for item in data["nodes"]:
+                        if item["ip"] == ip_dest:
+                            data["egdges"].append({"from":evt["id"],"to":item["id"],"color":"red"})
+                            break
+
+    
+
+    return data
+    
+
+
+
+
+if __name__ == "__main__":
+    es = Elasticsearch(cloud_id=CLOUD_ID,http_auth=("elastic",PASSWORD))
+    print(asyncio.run(retrieve_netwrok_tpoplogy(es)))
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from decouple import config
 from elasticsearch import Elasticsearch
-from ..pzero.Elk import event_searching
+from Elk import event_searching
+from fetch_events import retrieve_netwrok_tpoplogy
 app = FastAPI()
 
 CLOUD_ID = config("CLOUD_ID")
 PASSWORD = config("ELASTIC_PASSWORD")
+
 
 @app.get("/")
 async def index():
@@ -13,12 +15,6 @@ async def index():
     if CLOUD_ID and PASSWORD:
         data ={"nodes":[],"edges":[]} # machines network data
         es = Elasticsearch(cloud_id=CLOUD_ID,http_auth=("elastic",PASSWORD))
-        query = {
-            "bool":{
-                "must":[{"match":{"event.code":"3"}}],
-                "filter":[]
-            }
-        }
-        print(event_searching(es,query=query))
+        return await retrieve_netwrok_tpoplogy(es)
     else:
         return {"network_data":None}
