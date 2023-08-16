32b4f04a479f158a1c22ce3bdd8566dfed743145
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-08-16 22:21:18 +0100

    Update db connection function and search_event

--- backend/main.py
+++ backend/main.py
@@ -32,10 +32,18 @@ app.add_middleware(
 CLOUD_ID = Config("CLOUD_ID")
 PASSWORD = Config("ELASTIC_PASSWORD")
 
+def connect_es(db: Session = Depends(get_db)):
+    creds = get_creds(id=1,db=db)
+    if creds:
+        es = Elasticsearch(cloud_id=creds.apikey,http_auth=(creds.username,creds.password))
+    elif CLOUD_ID and PASSWORD:
+        creds_obj = ElasticCreds(id=1,apikey=CLOUD_ID,username="elastic",password=PASSWORD)
+        creds = create_creds(db=db,credsItem=creds_obj)
+        es= Elasticsearch(cloud_id=CLOUD_ID,http_auth=("elastic",PASSWORD))
+    return es    
 
 @app.get("/")
 async def index(db: Session = Depends(get_db)):
-    global es
     data ={"nodes":[],"edges":[]} # machines network data
     try:
         # check for creds in db 
@@ -60,26 +68,27 @@ async def elastic(creds : ElasticCreds, db: Session = Depends(get_db)):
     obj = get_creds(db,creds.id)
     if obj:
         updated_obj = update_creds(db,creds)
-        global es
-        es = Elasticsearch(cloud_id=creds.apikey,http_auth=(creds.username,creds.password))
         return creds
     else:
         obj=create_creds(db,creds)
     return obj
 
 @app.get("/search")
-async def search_event(event_code: int ,username: str | None = None,ip_address: str | None = None, event_date_start : str | None = None, event_date_end : str | None = None):
+async def search_event(event_code: int ,username: str | None = None,ip_address: str | None = None, event_date_start : str | None = None, event_date_end : str | None = None,db: Session = Depends(get_db)):
+
+    es = connect_es(db)
+
     search_query = {
         "bool":{
             "must":[
-                {"match":{"event.code":str(event_code)}}
+                {"match":{"event.code":event_code}}
             ],
             "filter":[]
         }
     }
-    if username:
+    if username != None:
         search_query["bool"]["must"].append({"match":{"winlog.user.name":username}})
-    if ip_address:
+    if ip_address != None:
         search_query["bool"]["must"].append({"match":{"host.ip":ip_address}})
     
     if event_date_start or event_date_end:
@@ -90,8 +99,7 @@ async def search_event(event_code: int ,username: str | None = None,ip_address: 
                 "lte":event_date_end
             }
         }})
-
-    return event_searching(es,search_query)
+    return event_searching(es=es,query=search_query)
 
 
 @app.post("/pzero")
