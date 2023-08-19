64077ca6a947823abe7fdeea75328dd41af96121
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-08-19 23:07:17 +0100

    consume API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import SideBar from './components/SideBar';
 import ToolBar from './components/ToolBar';
 import NetView from './components/NetView';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 //["FFFFFF","0457A6","07539D","FFFFFF","162360"]
 function App() {
   const [selected,setSelected] = useState(false);
+  const [networkData,setNetworkData] = useState(null) 
+  const fetchData = async()=>{
+    await fetch("")
+  } 
+  useEffect()
   return (
     <div className="App">
         <SideBar selected={selected} setSelected={setSelected}/>
---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const API_URL = {
+    dev: "http://locahost:8000"
+};
+
+export const DEBUG = "dev";
