0b04394458f98b38ba2b972bb5468bc6c0fd40bd
0xhunter213 <m.seddaoui@esi-sba.dz>
2023-08-31 21:01:08 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,12 +10,13 @@ import axios from "axios";
 function App() {
   const [selected,setSelected] = useState(false);
   const [data,setData] = useState({nodes:[],edges:[]});
+
   const fetchData = async () =>{
     await axios.get(`${API_URL[DEBUG]}`).then(
       response =>{
-        let nodes = response.data.nodes.filter((val,id,array) => {return array.indexOf(val) == id;})
-        let edges = response.data.edges
-        if(nodes.length != 0){
+        let nodes = response.data.nodes;
+        let edges = response.data.edges;
+        if(nodes.length !== 0){
           nodes.forEach(element => {
             edges.push({from:element.id,to:"elastic",color:"green"})
           });
@@ -31,9 +32,27 @@ function App() {
           )
          
         }
-        
-        setData({nodes:nodes?nodes:[],edges:edges?edges:[]})
-        console.log("data",data)
+
+        console.log({nodes:nodes?nodes:[],edges:edges?edges:[]})
+        if((data.nodes.length === 0 && nodes.length > 0) || (nodes.length > data.nodes.length)){
+          setData({nodes:nodes?nodes:[],edges:edges?edges:[]})
+          console.log("data",data)
+        }else{
+          if(nodes.length === data.nodes.length){
+            let change = false;
+            for(var i=0;i<nodes.length;i++){
+              let obj1=nodes[i];
+              let obj2=data[i];
+              if(obj1.id !== obj2.id){
+                change=true
+              }
+            }
+            if(change){
+              setData({nodes:nodes?nodes:[],edges:edges?edges:[]});
+            }
+          }
+        }
+
       }
     ).catch(e=>{
       setData(data)
